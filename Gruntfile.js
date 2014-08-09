f5aed8dc6f228e5db3132e96a974171bed9d7191
cleversoap <clever@cleversoap.com>
2014-08-10 09:40:03 +1000

    'npm run build' will now build a minified version using the grunt uglify target

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,26 @@ module.exports = function (grunt) {
 
 
     grunt.initConfig({
+        uglify: {
+            parg : {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: 'parg.min.map',
+                    preserveComments: 'some'
+                },
+                files: {
+                    'parg.min.js': ['parg.js']
+                }
+            }
+        },
         jasmine: {
             src: 'parg.js',
             options: {
-                specs: 'test/test-spec.js'
+                specs: 'test.js'
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
